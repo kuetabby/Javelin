34faa4f4a4c13318fbfb499fb383c9a7614d3c81
priaaji-seyanegoro <priaaji.setyanegoro@gmail.com>
2025-05-19 16:17:52 +0700

    updated

--- src/components/Loader/Custom.tsx
+++ src/components/Loader/Custom.tsx
@@ -2,19 +2,19 @@ import React from "react";
 import { kanit } from "@/utils/font";
 import Image from "next/image";
 import "./style.css";
-import loadingGif from "@/assets/loader.gif"
+import loadingGif from "@/assets/solnity-plain.png";
 
-interface Props { }
+interface Props {}
 
 const YinLoader: React.FC<Props> = () => {
   return (
     <div
       className={`w-full h-screen flex flex-col justify-center items-center mx-auto overflow-hidden ${kanit.className}`}
     >
-      {/* <div className="w-full h-28 flex items-center justify-center">
-        <div className="loader" />
-      </div> */}
       <div className="w-full h-28 flex items-center justify-center">
+        <div className="loader" />
+      </div>
+      {/* <div className="w-full h-28 flex items-center justify-center">
         <Image
           src={loadingGif}
           alt="Loading"
@@ -23,7 +23,7 @@ const YinLoader: React.FC<Props> = () => {
           width={20} // Adjust width as necessary
           height={20} // Adjust height as necessary
         />
-      </div>
+      </div> */}
     </div>
   );
 };
